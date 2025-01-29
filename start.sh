8327edfbbef7d9d6b4becc725e4d08ffba61e685
niles420 <420niles@gmail.com>
2025-01-28 19:12:40 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
 
-# Pull latest changes
+# Pull the latest changes
 git pull origin main
 
-# Build Docker images
+# Build and load the local Docker images
 docker build -t fppr-hive:latest -f Dockerfile.hive .
 docker build -t fppr-member:latest -f Dockerfile.member .
+
+# Load images into Minikube (if applicable)
+minikube image load fppr-hive:latest
+minikube image load fppr-member:latest
 
 # Apply Kubernetes configurations
 kubectl apply -f fppr-hive-deployment.yaml
 kubectl apply -f fppr-member-deployment.yaml
-kubectl apply -f fppr-service.yaml
 
-# Verify pods
+# Restart pods to use updated images
+kubectl delete pod -l app=fppr-hive
+kubectl delete pod -l app=fppr-member
+
+# Display running pods
 kubectl get pods
